e3171bc6651b62e81556082861660ddcebaf4d49
antony-lucide <antony.lucide@laplateforme.io>
2023-02-21 19:15:19 +0100

    Ceci est un prototype de script pour le projet pendu de la project pool sur python.

--- pendu.py
+++ pendu.py
@@ -53,11 +53,30 @@ def reload():
         total = mot()
         défaite = 0
 
+def Menu():
+    global total
+    total = input()
+    print(total)
+
+
+def window():
+    image = interface.blit(liste_image[défaite],(0,0))    
+    interface.blit(pendu,(580,200))
+    interface.blit(titre,(580,100))
+   
+
+def opti():
+    interface.blit(menu,(300,200))
+    py.draw.rect(menu,couleur_noir,[50, 30, 80 , 40], 0)
+    menu.blit(boutton,(50,30))
+    py.draw.rect(menu,couleur_noir,[50, 130, 80 , 40], 0)
+    menu.blit(entrybox,(30,90))
 
 #Une liste pour stocker les charactère rentré par l'utilisateur.
 liste_de_lettre = []
 
-#Interface.
+
+#Arborescence de l'interface.
 py.display.init()
 py.font.init()
 interface = py.display.set_mode((820,500))
@@ -70,22 +89,31 @@ couleur_active = py.Color('dark blue')
 pendu = text.render(string(total),False,'red')
 loose = text.render('défaite',True,'red')
 win = text.render('victoire',True,'red')
-option = text.render('échape pour exit',True,False)
+option = text.render('échape pour restart',True,False)
+menu = py.image.load('img/menu.jpeg').convert()
+menu = py.transform.smoothscale(menu,(200,200))
+couleur_noir = (200,200,200)
+boutton = text.render('Play',False,'red')
+entrybox = text.render('Entrybox',False,'red')
 #Liste contenant les images du pendu.
 liste_image = []
 #Variable pour la boucle infinie
 frame = True
 
+
+Menu()
 #Une boucle pour vérifier les images et pouvoir les formater en fonction de leur position.
 for i in range (7):
     liste_image += [py.image.load(f'img/pendu_{i}.png')]
+
+
 #La boucle principale pour faire tourner l'interface pygame sans intérference.
 #La boucle vérifie d'abord si l'utilisateur ferme l'interface ou non.
 #La deuxième condition vérifie si la touche que l'utilisateur a entrée est "KEYDOWN".
 #KEYDOWN, c'est-a-dire n'importe quel touches appuyer.
 #Si KEYDOWN est actif alors, afficher la lettre utiliser et afficher la lettre à la place de "_".
 #La dernière condition permet de recharger la partie.
-while frame:   
+while frame:
     for event in py.event.get():
         if event.type == py.QUIT:
             frame = False
@@ -95,21 +123,20 @@ while frame:
             pendu = text.render(string(total),False,'red')
             if event.key == py.K_ESCAPE:
                 reload()
+            if event.key == py.MOUSEBUTTONDOWN:
+                window()               
     #Condition pour vérifier si la partie est gagner et afficher "Victoire".
-    #Même condition pour la défaite, sauf que on vérifie le nombre d'image afficher.
+    #Même condition pour la défaite, sauf que on vérifie le nombre d'images afficher.
     if victorie():
         interface.blit(win,(580,300))
-        interface.blit(option,(530,400))
+        interface.blit(option,(160,400))
     if défaite > 5:
         interface.blit(loose,(620,400))
-        
     #Permet d'actualiser l'interface.
     py.display.update()
     interface.blit(interface_image,(0,0))
     #Condition pour recharger la page
     if défaite > 6:
         reload()
-    #Arborescence de l'interface.
-    interface.blit(liste_image[défaite],(0,0))    
-    interface.blit(pendu,(580,200))
-    interface.blit(titre,(580,100))
+    opti()
+    
